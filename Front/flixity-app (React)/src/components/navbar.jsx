9d52e7865acab16e72e3dbcd3178025c5cd50067
blumingann <38838075+blumingann@users.noreply.github.com>
2020-09-14 20:56:35 -0300

    .

--- Front/flixity-app (React)/src/components/navbar.jsx
+++ Front/flixity-app (React)/src/components/navbar.jsx
@@ -23,12 +23,12 @@ const NavBar = ({ user }) => {
           <NavLink className="nav-item nav-link" to="/movies">
             Movies
           </NavLink>
-          <NavLink className="nav-item nav-link" to="/customers">
+          {/* <NavLink className="nav-item nav-link" to="/customers">
             Customers
           </NavLink>
           <NavLink className="nav-item nav-link" to="/rentals">
             Rentals
-          </NavLink>
+          </NavLink> */}
 
           {!user && (
             <React.Fragment>
